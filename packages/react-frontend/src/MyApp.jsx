2edf129711ef95b69495e833ced87ff1bddc51c5
ChachaTeraSira <khushkaranpreet34@gmail.com>
2024-09-30 09:47:03 -0700

    Added useState, remove function, and created an array for data instead of hardcoding

--- packages/react-frontend/src/MyApp.jsx
+++ packages/react-frontend/src/MyApp.jsx
@@ -1,11 +1,40 @@
 // src/MyApp.jsx
-import React from "react";
+import React, { useState } from "react";
 import Table from "./Table";
 
 function MyApp() {
+  const [characters, setCharacters] = useState([
+    {
+      name: "Charlie",
+      job: "Janitor"
+    },
+    {
+      name: "Mac",
+      job: "Bouncer"
+    },
+    {
+      name: "Dee",
+      job: "Aspring actress"
+    },
+    {
+      name: "Dennis",
+      job: "Bartender"
+    }
+  ]);
+
+  function removeOneCharacter(index) {
+    const updated = characters.filter((character, i) => {
+      return i !== index;
+    });
+    setCharacters(updated);
+  }
+  
   return (
     <div className = "container">
-      <Table />
+      <Table 
+      characterData={characters}
+      removeCharacter={removeOneCharacter}
+      />
     </div>
     // ^ Browsers will not accept this kind of code directly, so we need Vite to transpile it to standard Javascript. (Technically, the name of the standard language is ECMAScript.)
   );
