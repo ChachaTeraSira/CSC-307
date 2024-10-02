ec392a1382f31d001d28485d7f90a9aad7e932d5
ChachaTeraSira <khushkaranpreet34@gmail.com>
2024-10-02 15:32:15 -0700

    Created a user found by ID

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -35,6 +35,20 @@ const port = 8000;
 
 app.use(express.json());
 
+const findUserById = (id) =>
+    users["users_list"].find((user) => user["id"] === id);
+  
+app.get("/users/:id", (req, res) => {
+    const id = req.params["id"]; //or req.params.id
+    let result = findUserById(id);
+    if (result === undefined) {
+        res.status(404).send("Resource not found.");
+    } else {
+        res.send(result);
+    }
+});
+  
+// FIND BY NAME 
 const findUserByName = (name) => {
     return users["users_list"].filter(
       (user) => user["name"] === name
