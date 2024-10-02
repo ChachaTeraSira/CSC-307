0e388b4102f23cafbccad081abadf99d1d7a0ce9
ChachaTeraSira <khushkaranpreet34@gmail.com>
2024-10-02 15:19:00 -0700

    installed nodeman and created a user found by name

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -35,13 +35,26 @@ const port = 8000;
 
 app.use(express.json());
 
-app.get("/users", (req, res) => {
-  res.send(users);
-});
+const findUserByName = (name) => {
+    return users["users_list"].filter(
+      (user) => user["name"] === name
+    );
+  };
+  
+  app.get("/users", (req, res) => {
+    const name = req.query.name;
+    if (name != undefined) {
+      let result = findUserByName(name);
+      result = { users_list: result };
+      res.send(result);
+    } else {
+      res.send(users);
+    }
+  });
 
 app.listen(port, () => {
   console.log(
-    `Example app listening at http://localhost:${port}`
+    `Example app listening at http://localhost:${port}/users`
   );
 });
 
